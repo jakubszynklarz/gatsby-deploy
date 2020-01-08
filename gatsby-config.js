e296d872f465bb0372685c4c7cb2cbf86545597f
jakubszynklarz <jakubszynklarz@gmail.com>
2020-01-08 17:04:52 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,47 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+    resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "evenbi10.vot.pl",
+        protocol: "https",
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        cookies: {},
+        verboseOutput: true,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://source-url.com",
+          replacementUrl: "https://replacement-url.com",
+        },
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        excludedRoutes: ["**/posts/1456"],
+        keepMediaSizes: false,
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`
   ],
 }
